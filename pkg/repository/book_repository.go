f8291baaeb371687719f279e94cc6b1ede20842c
Marcos Huck <marcos@huck.com.ar>
2021-01-17 01:18:02 -0300

    Add repositories.
    Signed-off-by: Marcos Huck <marcos@huck.com.ar>

---
+++ pkg/repository/book_repository.go
@@ -0,0 +1,42 @@
+package repository
+
+import (
+	"github.com/marcoshuck/streaming/pkg/model"
+	"gorm.io/gorm"
+	"log"
+)
+
+type Book interface {
+	Find(isbn *string) ([]model.Book, error)
+}
+
+type book struct {
+	db *gorm.DB
+}
+
+func (b *book) Find(isbn *string) ([]model.Book, error) {
+	var books []model.Book
+
+	q := b.db.Model(&model.Book{})
+
+	if isbn != nil {
+		q.Where("isbn = ?", *isbn)
+	}
+
+	if q.Find(&books).Error != nil {
+		return nil, q.Error
+	}
+
+	return books, nil
+}
+
+func NewBookRepository(db *gorm.DB) Book {
+	if db == nil {
+		log.Fatalln("db has not been provided")
+		return nil
+	}
+
+	return &book{
+		db: db,
+	}
+}
