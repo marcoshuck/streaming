8bda2f25ddbdea88d2e21f82382ac4f8cde7711b
Marcos Huck <marcos@huck.com.ar>
2021-01-17 01:20:11 -0300

    Add book controller.
    Signed-off-by: Marcos Huck <marcos@huck.com.ar>

---
+++ pkg/controller/book_controller.go
@@ -0,0 +1,50 @@
+package controller
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/marcoshuck/streaming/pkg/service"
+	"net/http"
+)
+
+type Book interface {
+	GetAll(w http.ResponseWriter, r *http.Request)
+	GetByISBN(writer http.ResponseWriter, request *http.Request)
+}
+
+type book struct {
+	service service.Book
+}
+
+func (b *book) GetByISBN(writer http.ResponseWriter, request *http.Request) {
+	panic("implement me")
+}
+
+func (b *book) GetAll(w http.ResponseWriter, r *http.Request) {
+	list, err := b.service.ListAll()
+	if err != nil {
+		http.Error(
+			w,
+			fmt.Sprintf("%s: %s", http.StatusText(http.StatusInternalServerError), err),
+			http.StatusInternalServerError,
+		)
+	}
+
+	bslice, err := json.Marshal(list)
+	if err != nil {
+		http.Error(
+			w,
+			fmt.Sprintf("%s: %s", http.StatusText(http.StatusInternalServerError), err),
+			http.StatusInternalServerError,
+		)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(bslice)
+}
+
+func NewBookController(service service.Book) Book {
+	return &book{
+		service: service,
+	}
+}
