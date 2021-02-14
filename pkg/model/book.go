518c36884d6e51de3d8345b661e0592a823ce8e6
Marcos Huck <marcos@huck.com.ar>
2021-02-14 16:31:51 -0300

    Add dataset.

--- pkg/model/book.go
+++ pkg/model/book.go
@@ -1,11 +1,16 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type Book struct {
 	gorm.Model
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	ISBN        string `json:"isbn"`
-	Author      string `json:"author"`
+	Title           string    `json:"title"`
+	Description     string    `json:"description"`
+	ISBN            string    `json:"isbn"`
+	Authors         string    `json:"authors"`
+	PublicationDate time.Time `json:"publication_date"`
+	Publisher       string    `json:"publisher"`
 }
